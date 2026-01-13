1143e798e85aa69b2c0931e9bf3c119ae634beac
solrac-obando <solracivan.aure@gmail.com>
2026-01-13 12:18:14 -0400

    Delete practicas_python_1 directory

--- practicas_python_1/holamundo/01-holamundo.py
+++
@@ -1,3 +0,0 @@
-print("########################################")
-print("Hola mundo soy Carlos Obando estoy repasando python para mejorar mis habilidades en programacion")
-print("##################2026##################")
--- practicas_python_1/practicas_python_1/variables_y_tipo_de_datos/05-curiosidades.py
+++
@@ -1,6 +0,0 @@
-mi_texto = "Master"
-mi_texto2 = " en \"Python\" "
-
-print(mi_texto + mi_texto2)
-# nota: para colocar las comillas en un string se debe de usar (\"   \"), no (/" este esta mal/")
-# el \n es un salto de linea, \t es una tabulacion
